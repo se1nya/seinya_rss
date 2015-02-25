1d5d6d87fe72919126d3c89673ab9c7a55ac8506
Seina Sakaue <seina.sakaue@asuka-holdins.com>
2015-02-25 20:53:15 +0900

    Postgress

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150225103849) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rsses", force: :cascade do |t|
     t.string   "url"
     t.string   "category"
